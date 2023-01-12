cd9433090b7a3759d7f77b84dbfbabd1dacf7e30
Kham Udom <khamu@outlook.com>
2023-01-11 21:24:39 -0800

    adjust image width and height and add hover to planBox

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -14,7 +14,7 @@ function Thumbnail({ movie }: Props) {
 
   return (
     <div
-      className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w[260px] md:hover:scale-105"
+      className="relative h-[101px] min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[261px] md:hover:scale-105"
       onClick={() => {
         setCurrentMovie(movie);
         setShowModal(true);
